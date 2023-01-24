e892e442b2fc67abc57de49cdb244c15f9a0198f
Yuri <amenoyuri1@gmail.com>
2023-01-24 19:21:23 -0300

    Listando filmes

---
+++ application/libraries/Tmdb.php
@@ -0,0 +1,23 @@
+<?php
+    class Tmdb{
+        private $url = 'https://api.themoviedb.org/3';
+        private $token = "44a8b8d3e5c7803c5edb90c090f5a018";
+        
+        public function listMoviesTopRated(){
+            $ch  = curl_init(); // Iniciar o CURL
+            curl_setopt($ch, CURLOPT_URL, $this->url."/movie/top_rated?api_key=".$this->token."&language=pt-BR"); // Aqui
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($ch);
+            $response = json_decode($response,true);
+            return $response;
+        }
+
+        public function listMoviesLatests(){
+            $ch  = curl_init(); // Iniciar o CURL
+            curl_setopt($ch, CURLOPT_URL, $this->url."/movie/top_rated?api_key=".$this->token."&language=pt-BR"); // Aqui
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($ch);
+            $response = json_decode($response,true);
+            return $response;
+        }
+    }
--- application/views/home.php
+++ application/views/home.php
@@ -1,25 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=base_url("assets/css/style.css") ?>">
+    <title>Netflix</title>
 </head>
-<body>
-   <ul>
-    <?php foreach($produtos as $key => $value){ 
-        
-    ?>
-        
-        <li><?= $value["nome"] ?> <a href="index.php/home/update?id=<?= $value["id"] ?>">Editar</a> <a href="index.php/home/delete?id=<?= $value["id"] ?>">Deletar</a></li>
 
-        
-        <?php 
-            } 
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Netflix</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="<?= base_url("") ?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url("lancamentos")?>">Lançamentos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            
+        <?php
+            foreach ($data['results'] as $value) {
         ?>
-        <br>
-    <a href="index.php/home/insert" class="button">Criar produto</a>
-   </ul>
+            <div class="col-md-3">
+                <div class="card mt-3 card-movie">
+                    <img src="https://image.tmdb.org/t/p/w500/<?= $value['poster_path']?>" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?= $value['title']?>
+                        </h5>
+                        <p>
+                            <?=$value['popularity'] ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php
+            }
+        ?>
+        </div>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
+
 </html>
